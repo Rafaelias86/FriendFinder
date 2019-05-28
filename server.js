2f24248c211b734bef0ebb38e88f5a474044c413
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-05-28 17:42:21 -0400

    Css linked

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// EXPRESS DATA PARSING CAPABILITIES
+// // For serving of static CSS
+// app.use(express.static(__dirname + "/app/public/css"));
+
+// EXPRESS DATA PARSING 
 app.use(express.static('app/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -18,6 +21,7 @@ app.use(express.static(__dirname + "/app/public/css"));
 // ROUTER
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 // LISTENER
 app.listen(PORT, function () {
